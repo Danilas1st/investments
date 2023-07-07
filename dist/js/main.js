3ce620ad24e59fb732591f283415cb1a5bb627f6
Danilas_1st <danil_tikhomirov98@mail.ru>
2023-07-07 11:13:45 +0300

    work

--- dist/js/main.js
+++ dist/js/main.js
@@ -20,6 +20,7 @@ $(window).on('load', function () {
 	updateSizes();
 	loadFunc();
 	showMoreText();
+	modal();
 });
 
 $(window).on('resize', function () {
@@ -215,3 +216,43 @@ const swiper_rev = new Swiper('.swiper_rev', {
     prevEl: '.reviews_slider_btn_prewios ',
   },
 });
+
+
+
+function modal() {
+	let popup = document.querySelectorAll('.popup')
+	let btnArray = document.querySelectorAll('.trigger')
+	
+	btnArray.forEach((el) => {
+		el.addEventListener('click', function(e) {
+			e.preventDefault();
+			let path = e.currentTarget.dataset.target
+			
+			popup.forEach((el) => {
+				if(el.dataset.id == path) {
+					isOpen(el)
+				}
+			})
+			
+		})
+	})
+	
+	popup.forEach((pop) => {
+		let remove = pop.querySelectorAll('.remove')
+		remove.forEach(el => {
+			el.addEventListener('click', (e) => {
+				isRemove(pop);
+			})
+		});
+	})
+}
+
+function isOpen(popup) {
+	document.body.classList.add('fixed')
+	popup.classList.add('active')
+}
+
+function isRemove(popup) {
+	popup.classList.remove('active')
+	document.body.classList.remove('fixed')
+}
